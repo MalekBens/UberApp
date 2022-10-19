253843aa229ae61c2f2a464fc613229296c5e208
MalekBens <bn.salah.malek@gmail.com>
2022-10-19 12:21:46 +0100

    Uber Mobile Application

---
+++ App.js
@@ -0,0 +1,65 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,PermissionsAndroid, Platform } from 'react-native';
+import DestinationMapScreen from './src/Screens/DestinationMapScreen/DestinationMapScreen';
+import HomeScreen from './src/Screens/HomeScreen'
+import DestinationScreen from './src/Screens/DestinationScreen/DestinationScreen'
+import { useEffect } from 'react';
+import * as Location from 'expo-location'
+import RootNavigation from './src/navigation/RootNavigation';
+// import { withAuthenticator } from 'aws-amplify-react-native';
+
+
+// import { Amplify } from 'aws-amplify'
+// import awsconfig from './src/aws-exports'
+// Amplify.configure({
+//   ...awsconfig,
+//   Analytics: {
+//     disabled: true,
+//   },
+// })
+
+ function App() {
+
+  const checkPermission = async()=>{
+    const hasPermission = await Location.requestForegroundPermissionsAsync();
+    if (hasPermission.status === 'granted') {
+        const permission = await askPermission();
+        return permission
+    }
+    return true
+};
+
+const askPermission = async()=>{
+    const permission = await Location.requestForegroundPermissionsAsync();
+    return permission.status === 'granted';
+};
+
+const getLocation = async()=>{
+    try {
+        const {granted} = await Location.requestForegroundPermissionsAsync();
+        if (!granted) {
+            return;
+        }
+        const {
+            coords:{latitude,longitude},
+        } = await Location.getCurrentPositionAsync();
+        setLatLng({latitude:latitude,longitude:longitude})
+    } catch (error) { 
+        
+    }
+}
+useEffect(()=>{
+  checkPermission();
+  getLocation(),[]
+})
+  return (
+    <>
+      <StatusBar barStyle="dark-content"/>
+      <RootNavigation/>
+    </>
+  );
+}
+
+export default App;
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,31 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "uberclone1582260615822606": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "hGroupRole": "string"
+        }
+    },
+    "function": {
+        "uberclone1582260615822606PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "uberclone": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
